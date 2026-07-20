b486975de01ecfd4b1a327993b1144c20a07b50e
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-20 02:38:57 -0500

    feat: implementar entity Usuario

--- src/main/java/com/jorgegmch/logitrack/entity/Usuario.java
+++ src/main/java/com/jorgegmch/logitrack/entity/Usuario.java
@@ -1,5 +1,40 @@
 package com.jorgegmch.logitrack.entity;
 
-public class Usuario {
+import com.jorgegmch.logitrack.entity.enums.Rol;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@EqualsAndHashCode(of = "idUsuario")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    @ToString.Exclude
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
 }
