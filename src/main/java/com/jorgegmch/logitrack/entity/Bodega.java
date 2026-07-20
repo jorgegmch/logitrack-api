39a4eb173afcc78f4cb7cb4df4840c4ec68c0a6d
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-20 03:29:37 -0500

    feat: implementar entity Bodega

--- src/main/java/com/jorgegmch/logitrack/entity/Bodega.java
+++ src/main/java/com/jorgegmch/logitrack/entity/Bodega.java
@@ -1,5 +1,39 @@
 package com.jorgegmch.logitrack.entity;
 
-public class Bodega {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "bodega")
+@Data
+@EqualsAndHashCode(of = "idBodega")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bodega {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idBodega;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String ubicacion;
+
+    @Column(nullable = false)
+    private Integer capacidad;
+
+    @ManyToOne
+    @JoinColumn(name = "encargado_id")
+    private Usuario encargadoId;
 }
