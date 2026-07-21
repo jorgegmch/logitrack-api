4963e901819386a177126dad16d0bb4b8c2b7cf6
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-21 02:18:42 -0500

    feat: implementar BodegaRepository

--- src/main/java/com/jorgegmch/logitrack/repository/BodegaRepository.java
+++ src/main/java/com/jorgegmch/logitrack/repository/BodegaRepository.java
@@ -1,5 +1,9 @@
 package com.jorgegmch.logitrack.repository;
 
-public class BodegaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jorgegmch.logitrack.entity.Bodega;
+
+public interface BodegaRepository extends JpaRepository<Bodega, Long> {
 
 }
