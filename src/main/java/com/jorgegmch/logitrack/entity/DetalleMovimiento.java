3b0b2fee78237b70bec5889a0d4d1212dd645a88
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-21 01:52:04 -0500

    feat: implementar entity DetalleMovimiento

--- src/main/java/com/jorgegmch/logitrack/entity/DetalleMovimiento.java
+++ src/main/java/com/jorgegmch/logitrack/entity/DetalleMovimiento.java
@@ -1,5 +1,37 @@
 package com.jorgegmch.logitrack.entity;
 
-public class DetalleMovimiento {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "detalle_movimiento")
+@Data
+@EqualsAndHashCode(of = "idDetMov")
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleMovimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetMov;
+
+    @ManyToOne
+    @JoinColumn(name = "movimiento_id", nullable = false)
+    private Movimiento movimiento;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto productoId;
+
+    @Column(nullable = false)
+    private Integer cantidad;
 }
