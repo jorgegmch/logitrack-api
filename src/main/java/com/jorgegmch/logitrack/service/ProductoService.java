36b9793e328697e0cb516f7a8a7011b26006d471
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-21 05:33:48 -0500

    feat: implementar ProductoService con CRUD completo

--- src/main/java/com/jorgegmch/logitrack/service/ProductoService.java
+++ src/main/java/com/jorgegmch/logitrack/service/ProductoService.java
@@ -1,5 +1,73 @@
 package com.jorgegmch.logitrack.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jorgegmch.logitrack.entity.Producto;
+import com.jorgegmch.logitrack.repository.ProductoRepository;
+
+@Service
 public class ProductoService {
+    private final ProductoRepository productoRepository;
+
+    ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    public List<Producto> listarProductos() {
+        return productoRepository.findAll();
+    }
+
+    public Producto buscarProductoPorId(Long id) {
+        Producto producto = productoRepository.findById(id).orElse(null);
+        if (producto == null) {
+            throw new RuntimeException("Producto no encontrado con id: " + id);
+        }
+        return producto;
+    }
+
+    public Producto crearProducto(String nombre, String categoria, BigDecimal precio) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El producto debe tener un nombre");
+        }
+        if (precio == null) {
+            throw new IllegalArgumentException("El producto debe tener un precio");
+        }
+        if (precio.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo");
+        }
+        String categoriaFinal = (categoria != null) ? categoria.trim().toUpperCase() : null;
+
+        return productoRepository.save(new Producto(null, nombre.trim().toUpperCase(), categoriaFinal, precio));
+    }
+
+    public Producto actualizarProducto(Long id, String nombre, String categoria, BigDecimal precio) {
+        Producto producto = buscarProductoPorId(id);
+
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El producto debe tener un nombre");
+        }
+        if (precio == null) {
+            throw new IllegalArgumentException("El producto debe tener un precio");
+        }
+        if (precio.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo");
+        }
+
+        String categoriaFinal = (categoria != null) ? categoria.trim().toUpperCase() : null;
+
+        producto.setNombre(nombre.trim().toUpperCase());
+        producto.setCategoria(categoriaFinal);
+        producto.setPrecio(precio);
+
+        return productoRepository.save(producto);
+    }
+
+    public void eliminarProducto(Long id) {
+        buscarProductoPorId(id);
+        productoRepository.deleteById(id);
+    }
 
 }
