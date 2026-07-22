83917b96656105f8f8bb04b81257313d2ca52012
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2026-07-22 14:56:22 -0500

    feat: implementar ApplicationContextProvider

--- src/main/java/com/jorgegmch/logitrack/config/ApplicationContextProvider.java
+++ src/main/java/com/jorgegmch/logitrack/config/ApplicationContextProvider.java
@@ -1,5 +1,20 @@
 package com.jorgegmch.logitrack.config;
 
-public class ApplicationContextProvider {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext contexto;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        contexto = applicationContext;
+    }
+
+    public static <T> T obtenerBean(Class<T> clase) {
+        return contexto.getBean(clase);
+    }
 }
